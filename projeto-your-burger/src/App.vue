e3641b332c639d81fb95e550db23301defd5de1c
ETI - Douglas Rodrigues Ferreira Campos <drcampos@sescpe.com.br>
2023-01-03 16:43:59 -0300

    VueJs

--- projeto-your-burger/src/App.vue
+++ projeto-your-burger/src/App.vue
@@ -1,26 +1,48 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
-
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+    <div>
+      <Navbar :logo="logo_src" :alt="app_name" />
+      <div>
+        <router-view></router-view>
+      </div>
+      <Footer />
+    </div>
+  </template>
+  <script>
+  import Navbar from './components/Navbar'
+  import Footer from './components/Footer'
+  
+  export default {
+    data: function() {
+      return {
+        logo_src: "/img/logo.png",
+        app_name: "Make Your Burger"
+      }
+    },
+    components: {
+      Navbar,
+      Footer
+    }
   }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  </script>
+  
+  <style>
+    * {
+      font-family: Helvetica;
+      padding: 0;
+      margin: 0;
+      box-sizing: border-box;
+    }
+  
+    .main-container {
+      margin: 50px;
+      min-height: 250px;
+    }
+  
+    h1 {
+      text-align: center;
+      font-size: 42px;
+      margin-bottom: 30px;
+      color: #222;
+    }
+  </style>
+  
